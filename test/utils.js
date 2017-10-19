ad8499313326e1c7284111f15d96a5c5b478f7dd
Andy Gray <andy@whatsthescore.co.uk>
2017-10-19 16:55:17 +0100

    better js

--- test/utils.js
+++ test/utils.js
@@ -3,12 +3,12 @@ module.exports = {
     try {
       await promise
     } catch (error) {
-      const invalidJump = error.message.search('invalid JUMP') >= 0
-      const invalidOpcode = error.message.search('invalid opcode') >= 0
-      const outOfGas = error.message.search('out of gas') >= 0
-      assert(invalidJump || invalidOpcode || outOfGas, "Expected throw, got '" + error + "' instead")
+      const invalidJump = error.message.search('invalid JUMP') >= 0;
+      const invalidOpcode = error.message.search('invalid opcode') >= 0;
+      const outOfGas = error.message.search('out of gas') >= 0;
+      assert(invalidJump || invalidOpcode || outOfGas, "Expected throw, got '" + error + "' instead");
       return
     }
     assert.fail('Expected throw not received')
   }
-}
+};
